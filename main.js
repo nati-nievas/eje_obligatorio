309dee26366e037b977468800fef741b8389a579
natalia <natalia_nievas91@hotmail.com>
2023-06-02 09:53:30 -0300

    mod formulario

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 function validar(event, form) {
     // Prevenir el envío del formulario
     event.preventDefault();
-  
+    
+    
+    
     // Obtener valores de los campos
     const nombre = form.nombre.value;
     const apellido = form.apellido.value;
@@ -10,44 +12,49 @@ function validar(event, form) {
     const categoria = form.categoria.value;
   
     // Realizar acciones con los datos
-    // Por ejemplo, mostrar los datos en la consola
+   
+    if (nombre == ""){
+       return alert("Debe ingresar nombre");
+    } 
+    if(apellido == ""){
+        return alert ("Debe ingresar apellido");
+    }
+    const re = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+     if (!re.test(form.mail.value)) {
+         alert("Debe ingresar un mail valido.");
+         return false;
+         }
+
+    if(cantidad == ""){
+        return alert("Debe seleccionar cantidad de tickets")
+    }
+    if(categoria == ""){
+        return alert("Debe seleccionar categoria");
+    }
+    else {
+       const cant= parseInt(cantidad)
+       const cat= parseFloat(categoria)
+       const resultado = (cantidad*200*categoria)
+       console.log(cant+cat);
+       document.getElementById("totalApagar").innerHTML= resultado;
+       
+     };
+    
+    
+    
+    
     console.log('Nombre:', nombre);
     console.log('Apellido:', apellido);
     console.log('Correo:', mail);
     console.log('Cantidad:', cantidad);
     console.log('Categoría:', categoria);
-  
-    // Realizar la validación
-    // ... Lógica de validación ...
-  
-    // Si la validación es exitosa, se realiza alguna acción adicional
-    if (validacionExitosa) {
-      // Realizar acciones adicionales, como enviar los datos a través de una API, mostrar un mensaje, etc.
-    }
   }
 
 
 
 
 
-    // if (formulario.nombre.value.trim().length == 0) {
-        // alert("Debe ingresar un nombre");
-        // return false;
-    // }
-    // if (formulario.apellido.value.trim().length == 0) {
-        // alert("Debe ingresar Apellido");
-        // return false;
-    // }
-    // const re = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-    // if (!re.test(formulario.mail.value)) {
-        // alert("Debe ingresar un mail valido.");
-        // return false;
-    // }
-    // if (formulario.cantidad.value == 0) {
-        // alert("Debe ingresar un numero.");
-        // return false;
-    // }
-    
+  
 
 
 
